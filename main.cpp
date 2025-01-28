8cbc1cb22e6b9d2bb76db083ab1fdd58988049cd
404SkillNotFound <shivang.upadhyay1@gmail.com>
2025-01-28 22:45:47 +0530

    fixed

--- main.cpp
+++ main.cpp
@@ -1,32 +1,30 @@
+//bubblesort algorithm
+
 #include <iostream>
 
 using namespace std;
-//bubblesort
-int* sortarr(int arr[]) {
-    int n = sizeof(arr)/sizeof(arr[0]);
-    for (int i=0;i<10;i++) {
-        for (int j=0;j<n;j++) {
-            if (arr[j] < arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-
-            }
-        }
-    }
-    return arr;
-
-}
-
-
+// Bubble Sort
 
 int main () {
 
-    int arr[]={1,3,4,5,98,4,7,45,23,56,9};
-    sortarr(arr);
-    for (int i=0;i<10;i++) {
+    int arr[] = {1, 3, 4, 5, 98, 4, 7, 45, 23, 56, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);  // Size of the array
+
+    // Bubble Sort Algorithm
+    for (int i = 0; i < n - 1; i++) {  // Outer loop for n-1 passes
+        for (int j = 0; j < n - i - 1; j++) {  // Inner loop to compare adjacent elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // Output the sorted array
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    return 1;
-
+    return 0;
 }
