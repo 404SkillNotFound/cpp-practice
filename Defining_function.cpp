8c15aeb8495401ace70525e68abfa45a544c5ff5
404SkillNotFound <shivang.upadhyay1@gmail.com>
2025-07-26 10:14:39 +0530

    Add Defining_function.cpp and Defining_function.exe

---
+++ Defining_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void doPrint() {
+  cout << "Hello World!" << endl;  // Definition of user-defined function doPrint()
+// doPrint() is the called function in this example
+  }
+  int main() {
+    cout<<"Starting main"<<endl; // Interrupt main() by making a function call to doPrint().  main() is the caller.
+    doPrint(); // This statement is executed after doPrint() ends
+
+    return 0;
+
+  }
