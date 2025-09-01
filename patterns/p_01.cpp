628bb223b8fa70230c5d6bb8eedecf76dee71030
404SkillNotFound <shivang.upadhyay1@gmail.com>
2025-09-01 18:26:30 +0530

    Pattern_1

---
+++ patterns/p_01.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hp on 31-08-2025.
+//
+
+#include "p_01.h"
---
+++ patterns/p_01.h
@@ -0,0 +1,16 @@
+//
+// Created by hp on 31-08-2025.
+//
+
+#ifndef P_01_H
+#define P_01_H
+
+
+
+class p_01 {
+
+};
+
+
+
+#endif //P_01_H
