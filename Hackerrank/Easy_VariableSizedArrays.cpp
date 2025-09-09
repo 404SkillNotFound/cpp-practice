97cdfd696c298e9e96f7ea68df409bbfcae5fb4b
404SkillNotFound <shivang.upadhyay1@gmail.com>
2025-09-09 18:37:34 +0530

    Initial 2 loops

--- Hackerrank/Easy_VariableSizedArrays.cpp
+++ Hackerrank/Easy_VariableSizedArrays.cpp
@@ -1,3 +1,27 @@
-//
-// Created by hp on 07-09-2025.
-//
+//Problem Link: https://www.hackerrank.com/challenges/variable-sized-arrays/problem?isFullScreen=true
+//Create Array of Arrays - use 2d vectors
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+  int n; int q; cin >> n >> q;
+  vector<vector<int>> a(n);
+  for(int i = 0; i < n; i++) {
+    int k; cin >> k;
+    a[i].resize(k);
+    for (int j = 0; j < k; j++) {
+      cin >> a[i][j];
+    }
+
+
+
+  }
+
+    return 0;
+}
