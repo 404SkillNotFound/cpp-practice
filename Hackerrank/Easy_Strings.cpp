1eb0bcf2d7dcdbe07ffe9a791d2ac84630cd92a3
404SkillNotFound <shivang.upadhyay1@gmail.com>
2025-12-23 17:04:10 +0530

    add string problem

---
+++ Hackerrank/Easy_Strings.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a { };
+    string b { };
+
+    cin >> a;
+
+    cin >> b;
+
+    int len_a = a.size();
+    int len_b = b.size();
+    cout << len_a << " " << len_b << endl;
+
+    cout<< a + b << endl;
+    char temp;
+    char c0 = a[0];
+    char c1 = b[0];
+    temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+
+    cout << a << " " << b << endl;
+
+
+    return 0;
+}
